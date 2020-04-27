68d715eb8dcd65298d533fd4149610b0749a7cde
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-27 17:57:22 -0300

    Exercícios finalizados e .gitignore adicionado.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,21 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+func find<Element: Comparable>(element: Element, on array: [Element]) -> Bool {
+    return array.contains(element)
+}
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+extension Array where Element: Numeric {
+    func sum() -> Element {
+        return self.reduce(Element.zero, { $0 + $1 })
+    }
+}
+
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
-    //BONUS TODO: Fazer o método acima receber N arrays
+//BONUS TODO: Fazer o método acima receber N arrays
+func numberOfCommonItems(on arrays: [Array<Any>]) -> Int {
+    var count = 0
+    
+    return count
+}
